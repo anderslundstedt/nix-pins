8c7a5bbeb6d82815879f2f8af977ffbe237e0100
Anders Lundstedt <git@anderslundstedt.se>
2026-08-23 20:36:59 +0200

    flake.nix and flake.lock

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixos-24-11.url           = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-darwin-24-11.url  = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nixos-25-11.url           = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-darwin-25-11.url  = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+    nixos-stable.url          = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs-darwin-stable.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
+    nixpkgs-unstable.url      = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nix-darwin.url                    = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin-stable";
+
+    home-manager-darwin.url                    = "github:nix-community/home-manager";
+    home-manager-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin-stable";
+    home-manager-linux.url                     = "github:nix-community/home-manager";
+    home-manager-linux.inputs.nixpkgs.follows  = "nixos-stable";
+
+    nix-homebrew.url          = "github:zhaofengli/nix-homebrew";
+    homebrew-core.url         = "github:homebrew/homebrew-core";
+    homebrew-core.flake       = false;
+    homebrew-cask.url         = "github:homebrew/homebrew-cask";
+    homebrew-cask.flake       = false;
+  };
+  outputs = inputs@{...}: {
+    inherit inputs;
+  };
+}
